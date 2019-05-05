05efd8c2b7955336bbe05f93ca6ab95996f852c5
abhijiths362 <abhijiths0098@gmail.com>
2019-05-05 11:56:50 +0000

    Trying to restore poweroff charging animation

--- device.mk
+++ device.mk
@@ -139,6 +139,11 @@ PRODUCT_PACKAGES += \
     liboverlay \
     libtinyxml
 
+# Healthd
+PRODUCT_PACKAGES += \
+    android.hardware.health@1.0-impl \
+    android.hardware.health@1.0-service \
+    chargeonlymode
 
 # OneplusShit
 PRODUCT_PACKAGES += \
